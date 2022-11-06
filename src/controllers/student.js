460900ded04e6ca32ed4c3c29a5946d6ab76d5fe
Ankit sharma <ankit.sharma09547@gmail.com>
2022-11-06 10:45:07 +0530

    delete drive

--- src/controllers/student.js
+++ src/controllers/student.js
@@ -4,6 +4,7 @@ import response from "../services/response/response.js";
 export const getStudents = async (req, res) => {
   try {
     const allStudents = await StudentData.find();
+    console.log(allStudents);
     response.succesResponse(res, 200, allStudents);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -18,6 +19,66 @@ export const createStudent = async (req, res) => {
     response.succesResponse(res, 201, newStudent);
   } catch (error) {
     res.status(409).json({ message: error.message });
+  }
+};
+
+export const deleteStudent = async (req, res) => {
+  // res.send('Router is working');
+  try {
+    console.log(req.query.id);
+    const deleteStudentResp = await StudentData.findByIdAndDelete(req.query.id);
+    response.succesResponse(res, 200, deleteStudentResp);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const editStudent = async (req, res) => {
+  const student = req.body;
+  console.log(req.body);
+
+  try {
+    const foundStudent = await StudentData.findOne({ _id: student.id });
+
+    console.log(foundStudent);
+    let myquery = { _id: student.id };
+    let newvalues = {
+      $set: {
+        date: student.date,
+        studentName: student.studentName,
+        vaccineStatus: student.vaccineStatus,
+        vaccineName: student.vaccineName,
+      },
+    };
+    const updatedDrive = await StudentData.updateOne(myquery, newvalues);
+    res.status(201).json(updatedDrive);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const studentStatusUpdate = async (req, res) => {
+  const student = req.body;
+
+  console.log(student);
+
+  try {
+    const foundStudent = await StudentData.findOne({ _id: student.id });
+
+    console.log(foundStudent);
+    let myquery = { _id: student.id };
+    let newvalues = {
+      $set: {
+        date: Date.now(),
+        studentName: foundStudent.studentName,
+        vaccineStatus: true,
+        vaccineName: foundStudent.vaccineName,
+      },
+    };
+    const updatedStudent = await StudentData.updateOne(myquery, newvalues);
+    res.status(201).json(updatedStudent);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
   }
 };
 
--- src/routes/student.js
+++ src/routes/student.js
@@ -3,13 +3,19 @@ import {
   getStudents,
   createStudent,
   uploadStudents,
+  deleteStudent,
+  editStudent,
+  studentStatusUpdate,
 } from "../controllers/student.js";
 
 const router = express.Router();
 
 router.get("/", getStudents);
-
 router.post("/", createStudent);
+router.put("/", editStudent);
+
+router.delete("/delete", deleteStudent);
+router.put("/statusUpdate", studentStatusUpdate);
 router.post("/upload", uploadStudents);
 
 export default router;
